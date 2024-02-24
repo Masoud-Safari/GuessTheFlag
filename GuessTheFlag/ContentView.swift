63f5980774928b641c1035a2213bd5352ae9c1af
Masoud Safari <mmsafari@gmail.com>
2024-02-23 16:40:52 -0800

    Add player score and final message.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,17 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let totalNumberOfRounds = 8
+    
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    @State private var scoreMessage = ""
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
     
     @State private var correctAnswer = Int.random(in: 0...2)
-    
+    @State private var totalScore = 0
+    @State private var round = 0
+    @State private var endOfGmae = false
     
     var body: some View {
+        
         ZStack {
-//            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
             RadialGradient(stops: [
                 .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
                 .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
@@ -60,9 +66,14 @@ struct ContentView: View {
                 .clipShape(RoundedRectangle(cornerRadius: 50))
                 
                 Spacer()
+                
+                Text("Round \(round + 1)")
+                    .foregroundStyle(.white)
+                    .font(.title2)
+                
                 Spacer()
                 
-                Text("Score ???")
+                Text("Score \(totalScore)")
                     .foregroundStyle(.white)
                     .font(.title.bold())
                 
@@ -73,24 +84,43 @@ struct ContentView: View {
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ???")
+            Text(scoreMessage)
+        }
+        .alert("Done", isPresented: $endOfGmae) {
+            Button("Start a new game") {
+                totalScore = 0
+                round = 0
+                countries.shuffle()
+                correctAnswer = Int.random(in: 0...2)
+            }
+        } message: {
+            Text("You guessed \(totalScore) \(totalScore == 1 ? "flag" : "flags") correctly out of \(totalNumberOfRounds).")
         }
     }
     
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            scoreMessage = "You chose the correct flag."
+            totalScore += 1
         } else {
             scoreTitle = "Wrong"
+            scoreMessage = "You chose the \(countries[number]) flag."
         }
         
         showingScore = true
     }
     
     func askQuestion() {
-        countries.shuffle()
-        correctAnswer = Int.random(in: 0...2)
+        if round + 1 == totalNumberOfRounds {
+            endOfGmae = true
+        } else {
+            round += 1
+            countries.shuffle()
+            correctAnswer = Int.random(in: 0...2)
+        }
     }
+    
 }
 
 #Preview {
